b4b5f54abd24b15d066cd82631289f55554202db
Wiktuar <wiktuar@yandex.ru>
2020-08-16 14:43:21 +0300

    fird commit

---
+++ src/teachproject/Car.java
@@ -0,0 +1,11 @@
+package teachproject;
+
+public class Car {
+    private String color;
+    private int amount;
+
+    public Car(String color, int amount) {
+        this.color = color;
+        this.amount = amount;
+    }
+}
